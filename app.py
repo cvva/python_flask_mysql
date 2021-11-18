fa0f88fd68626c6785e876af02398fe8a22d6c7a
Carlos Veliz de Villa <cvvaecc@gmail.com>
2021-11-17 22:41:06 -0500

    Agregando

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return '<h1>Trabajando con GIT</h1>'
 
+@app.route('/agregar')
+def add():
+	return '<h1>Agregando...</h1>'
+
 if __name__ == '__main__':
 	app.run(host='localhost', port=8080, debug=True)
