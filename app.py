08d92e103a377201917b6b67e7a0bc10d9e1c4b2
Carlos Veliz de Villa <cvvaecc@gmail.com>
2021-11-17 22:20:53 -0500

    Bienvenido a GIT

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+	return '<h1>Trabajando con GIT</h1>'
+
 if __name__ == '__main__':
 	app.run(host='localhost', port=8080, debug=True)
